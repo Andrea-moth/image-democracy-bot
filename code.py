17b998f2e145e7a11ab35babc36414162b3620ca
Andrea-moth <103472619+Andrea-moth@users.noreply.github.com>
2022-06-07 21:24:47 +0800

    Update code.py

--- code.py
+++ code.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.members = True
 
-botId = "OTY4NzQzMDQ1MzgwMDUxMDI2.GlFpyh.FCBk2PGoVn0hUF6lARWbipNj7mJMeqBYcJ38FE"
+botId = ""
 bot = commands.Bot(command_prefix="+", intents=intents)
 debug = Console()
 
